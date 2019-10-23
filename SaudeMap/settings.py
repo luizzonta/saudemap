00a73e41c8a78ed87c677746302a591d87e8f94f
luizzonta@hotmail.com <luizzonta@hotmail.com>
2019-10-23 20:47:17 -0300

    commit'

--- SaudeMap/settings.py
+++ SaudeMap/settings.py
@@ -122,11 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-<<<<<<< HEAD
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 import django_heroku
 django_heroku.settings(locals())
-
-=======
->>>>>>> 9dd8e9cbad07ab001800d70d178e8cd7ebe85953
