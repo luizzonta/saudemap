0b4c56e43c84c066fa037beaaf8a7cdca65b3f69
Luiz <Luiz@Zonta>
2019-10-24 22:14:36 -0300

    Atualização pagina index

--- SaudeMap/settings.py
+++ SaudeMap/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'c#e^8-73rwh3^37f2zqnjo*1cqoddiw3!kks-y5dj-4r1p0b!s'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost',
-    'saudemap.herokuapp.com'
+#    'localhost',
+#    'saudemap.herokuapp.com'
 ]
 
 # Application definition
