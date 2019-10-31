45950a38cbcfac4a81e4f708f07d383c88148a2b
aluno <aluno@lab2-pc07.local>
2019-10-30 22:04:06 -0300

    Criação Ficha Individual

--- AppSaudeMap/urls.py
+++ AppSaudeMap/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
-from AppSaudeMap.views import Index
+from AppSaudeMap.views import Index, CadastroIndividual
+from SaudeMap.models import FichaDoenca
 
 urlpatterns = [
     path('',Index.as_view(), name="index"),
+    path('saudemap/cadastroIndividual', CadastroIndividual.as_view(model=FichaDoenca), name="cadastro_individual"),
 ]
--- AppSaudeMap/views.py
+++ AppSaudeMap/views.py
@@ -1,5 +1,15 @@
 from django.views.generic import TemplateView
 from SaudeMap.models import FichaDoenca
+from django.contrib.messages.api import success
+from audioop import reverse
+from django.urls.base import reverse_lazy
+from django.views.generic.edit import CreateView
 
 class Index(TemplateView):
     template_name = "index.html"
+
+class CadastroIndividual(CreateView):
+    template_name = "cadastroIndividual.html"
+    model = FichaDoenca()
+    fields = '__all__'
+    success_url = reverse_lazy("index")    
