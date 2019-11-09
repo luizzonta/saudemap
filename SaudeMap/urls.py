67fba3364885ea4a36e2cccb598f24c67d26d5e3
aluno <aluno@lab1-pc29.local>
2019-11-08 22:32:26 -0200

    Atualização 08/11

--- SaudeMap/urls.py
+++ SaudeMap/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path('saudemap/', include('AppSaudeMap.urls')),
+    path('SaudeMap/', include('AppSaudeMap.urls')),
     path('admin/', admin.site.urls),
 ]
