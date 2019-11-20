439a0cf3c0267bb07d8f98067a2771e69ccbc48b
Luiz <Luiz@Zonta>
2019-11-19 23:49:38 -0300

    Atualização 19/11

--- SaudeMap/settings.py
+++ SaudeMap/settings.py
@@ -120,10 +120,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-#DEFAULT_CHARSET="iso-8859-1"
+DEFAULT_CHARSET="iso-8859-1"
 
- Static files (CSS, JavaScript, Images)
- https://docs.djangoproject.com/en/2.2/howto/static-files/
+ #Static files (CSS, JavaScript, Images)
+ #https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
