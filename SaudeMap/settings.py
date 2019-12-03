22eba490c1c57939bd7b773b6ecc903093366e8b
Lzonta <Lzonta@NOTE_LZONTA.ffr.grfischer.local>
2019-12-03 13:50:37 -0200

    atualização 03/12

--- SaudeMap/settings.py
+++ SaudeMap/settings.py
@@ -138,7 +138,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #Adicionar para HEROKU
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
 
 
 #Static files (CSS, JavaScript, Images)
